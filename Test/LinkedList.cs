323c05c8ab9471c4c2a8aad27c9a56d439875821
Alex Jienexa <alexjienexa@gmail.com>
2023-07-25 23:09:13 +0300

    Создан Node и LnkedList.Add

---
+++ Test/LinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LinkedList<T>
+{
+    Node<T>? head;
+    Node<T>? tail;
+    int count;
+
+
+    public void Add(T data)
+    {
+        Node<T> node = new Node<T>(data);
+        if (head == null)
+            head = node;    // Список пуст, а значит head и tail указывают на
+                            // одного и того же Node
+        else
+            tail!.Next = node; // ! = tail НЕ может быть null, потому что if
+        tail = node;
+        count++;
+    }
+}
---
+++ Test/Node.cs
@@ -0,0 +1,11 @@
+﻿public class Node<T>
+{
+    public Node(T data)
+    {
+        Data = data;
+    }
+
+    public T Data { get; set; }
+    public Node<T>? Next { get; set; }  // Значение, которому присваивается null
+                                        // При создании, если это возможно
+}
